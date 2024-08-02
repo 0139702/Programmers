198158aa7e8fd3ad8495ab49700866875e072939
0139702 <0139702@naver.com>
2024-08-02 19:49:36 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    for(int i=0; i<myString[i]; i++) {
+        myString[i] = tolower(myString[i]);   
+    }  
+    string answer = myString;
+    return answer;
+}
